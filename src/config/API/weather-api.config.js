3263499ddf99fce6127eb6c4dc7ff1b96ede8459
TOR <52118178+fredzerpa@users.noreply.github.com>
2021-08-03 00:00:41 -0400

    Add Custom Weather API methods and Cities List for Searchbar

--- src/config/API/weather-api.config.js
+++ src/config/API/weather-api.config.js
@@ -5,12 +5,26 @@ import axios from 'axios';
 import env from 'react-dotenv';
 
 export const getForecast = {
-  fiveDaysThreeHours: async query =>
+  fiveDaysThreeHours: {
+    byCity: async query =>
     await axios.get(`http://api.openweathermap.org/data/2.5/forecast`, {
       params: { q: query, appid: env.WEATHER_API_KEY },
     }),
+    byCoords: async ({lat, lon}) => 
+    await axios.get(``)
+  },
   currentWeather: async query =>
     await axios.get(`http://api.openweathermap.org/data/2.5/weather`, {
       params: { q: query, appid: env.WEATHER_API_KEY },
     }),
 };
+
+export const getOpenWeatherImage = async (codeOpenWeatherImage, size=2) => {
+  const response = await axios.get(`http://openweathermap.org/img/wn/${codeOpenWeatherImage}@${size}x.png`, {
+    responseType: 'arraybuffer'
+  });
+  const buffer = Buffer.from(response.data, 'binary').toString('base64')
+  console.log(buffer);
+  
+  return buffer;
+}
