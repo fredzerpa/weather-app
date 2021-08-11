2cdde477a87651903b39a976c582efbb39d406b7
TOR <52118178+fredzerpa@users.noreply.github.com>
2021-08-11 18:18:28 -0400

    Implement Material-UI Styles on App Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { useState } from 'react';
 
 // CSS
-import './App.scss';
+import './App.css';
+
+// * Libraries
+// + Material-UI
+import { makeStyles } from '@material-ui/core';
 
 // * Components
 import SearchBar from './components/searchbar/searchbar.component';
 import CardsOverview from './components/cards-overview/cards-overview.component';
 import GithubSticker from './components/github-sticker/github-sticker.component';
 
+import background from './assets/weather-backgrounds/snow-mountains.jpg';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100vw',
+    height: '100vh',
+    background:
+      `#000000 url(${background}) no-repeat center center`,
+    backgroundSize: 'cover',
+    overflowY: 'auto',
+  },
+});
+
 const App = () => {
   const [city, setCity] = useState('');
   const [geoLocation, setGeoLocation] = useState(null);
   const [weather, setWeather] = useState(null);
 
+  // -- Material Style
+  const classes = useStyles();
+  
   console.log(geoLocation);
 
   return (
-    <div className='App'>
+    <div className={classes.root}>
       <SearchBar setGeoLocation={setGeoLocation} />
-      <CardsOverview />
-
+      <CardsOverview geoLocation={geoLocation}/>
       <GithubSticker link={process.env.REACT_APP_GITHUB_ACCOUNT} />
     </div>
   );
