9894a2b1dde693935f1774357ecd964da6efa112
TOR <52118178+fredzerpa@users.noreply.github.com>
2021-08-16 00:06:58 -0400

    Update Overview Component to render 1 Forecast Card per day Taking into account that at noon (12pm) will be the main forecast, so the overview will take the 12 o' clock forecast, and pass the data to the Forecast Cards component.

--- src/components/cards-overview/cards-overview.component.js
+++ src/components/cards-overview/cards-overview.component.js
@@ -6,6 +6,19 @@ import { Grid, makeStyles } from '@material-ui/core';
 
 // * Components
 import ForecastCard from '../forecast-card/forecast-card.component';
+// + Icons
+import {
+  ClearNight,
+  Cloudy,
+  CloudyWithLightning,
+  CloudyWithMoon,
+  CloudyWithRainAndLightning,
+  CloudyWithSun,
+  Rainy,
+  Snowy,
+  Sunny,
+  SunnyWithWind,
+} from '../weather-icons/weather-icons.component';
 
 // -- Material-UI Styles
 const useStyles = makeStyles({
@@ -17,9 +30,49 @@ const useStyles = makeStyles({
   },
 });
 
-const weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+const WEATHER_ICONS_MATCH = {
+  // -- Group 2xx: Thunderstorm
+  Thunderstorm: {
+    211: <CloudyWithLightning />, // Thunderstorm without Rain
+    default: <CloudyWithRainAndLightning width='150' />,
+  },
+  // -- Group 3xx: Drizzle
+  Drizzle: {
+    default: <Rainy width='150' />,
+  },
+  // -- Group 5xx: Rain
+  Rain: {
+    default: <Rainy width='150' />,
+  },
+  // -- Group 6xx: Snow
+  Snow: {
+    default: <Snowy width='150' />,
+  },
+  // -- Group 7xx: Atmosphere
+  Atmosphere: {
+    default: null,
+  },
+  // -- Group 800: Clear
+  Clear: {
+    800: {
+      // Clear
+      day: <Sunny width='150' />,
+      night: <ClearNight width='150' />,
+    },
+    default: <Sunny width='150' />,
+  },
+  // -- Group 80x: Clouds
+  Clouds: {
+    801: {
+      // Few clouds
+      day: <CloudyWithSun width='150' />,
+      night: <CloudyWithMoon width='150' />,
+    },
+    default: <Cloudy width='150' />,
+  },
+};
 
-const CardsOverview = () => {
+const CardsOverview = ({ geoLocation: { data } }) => {
   const classes = useStyles();
   return (
     <Grid
@@ -28,11 +81,33 @@ const CardsOverview = () => {
       justifyContent='center'
       className={classes.container}
     >
-      {weekdays.map((day, i) => (
-        <Grid key={i} item xs={12} sm={4} lg={2}>
-          <ForecastCard day={day} codeOpenWeatherImage={'10d'} />
-        </Grid>
-      ))}
+      {data?.list
+        ?.filter(forecast => new Date(forecast.dt_txt).getHours() === 12)
+        .map((forecast, i) => {
+          const weather = forecast.weather[0];
+          const weatherCondition = weather.main;
+          const weatherCode = weather.id;
+          const weatherTime =
+            6 <= new Date(forecast.dt_txt).getHours() <= 18 ? 'day' : 'night';
+
+          return (
+            <Grid key={i} item xs={6} sm={4} lg={2}>
+              <ForecastCard
+                data={forecast}
+                icon={
+                  // If there's no Icon Code then display default
+                  // Taking into account if it's day or night time
+                  // ! the order is important
+                  WEATHER_ICONS_MATCH[weatherCondition]?.[weatherCode]?.[
+                    weatherTime
+                  ] ??
+                  WEATHER_ICONS_MATCH[weatherCondition]?.[weatherCode] ??
+                  WEATHER_ICONS_MATCH[weatherCondition].default
+                }
+              />
+            </Grid>
+          );
+        })}
     </Grid>
   );
 };
