4f3ebe491e10375185eb39c33e5f30ca4600735c
TOR <52118178+fredzerpa@users.noreply.github.com>
2021-09-03 05:11:18 -0400

    Add Header Component

---
+++ src/components/Header/header.component.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// * Libraries
+// + Material-UI
+import { Container, makeStyles } from '@material-ui/core';
+
+// * Components
+import SearchBar from '../searchbar/searchbar.component';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    background: '#3a3a3a',
+    boxShadow:
+      '0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)',
+  },
+}));
+
+const Header = props => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.container} maxWidth={false}>
+      <SearchBar {...props} />
+    </Container>
+  );
+};
+
+export default Header;
