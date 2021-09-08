24b8eed096ae520d5b7ca87f7e9092c7ec010b95
TOR <52118178+fredzerpa@users.noreply.github.com>
2021-09-08 18:17:53 -0400

    Add Open Weather API, for Weather forcasts

---
+++ src/config/API/open-weather/open-weather.api.js
@@ -0,0 +1,43 @@
+// * Libraries
+// + Axios
+import axios from 'axios';
+// + Environment Variables
+import env from 'react-dotenv';
+
+export const getForecast = {
+  fiveDaysThreeHours: {
+    byCity: async (cityName = 'london') => {
+      try {
+        return await axios.get(
+          `http://api.openweathermap.org/data/2.5/forecast`,
+          {
+            params: { q: cityName, appid: env.WEATHER_API_KEY, units: 'imperial' },
+          }
+        );
+      } catch (error) {
+        throw error;
+      }
+    },
+    byCoords: async ({ lat, lon }) => {
+      try {
+        return await axios.get(
+          `http://api.openweathermap.org/data/2.5/forecast`,
+          {
+            params: { lat, lon, appid: env.WEATHER_API_KEY, units: 'imperial' },
+          }
+        );
+      } catch (error) {
+        throw error;
+      }
+    },
+  },
+  currentWeather: async query => {
+    try {
+      return await axios.get(`http://api.openweathermap.org/data/2.5/weather`, {
+        params: { q: query, appid: env.WEATHER_API_KEY, units: 'imperial' },
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+};
