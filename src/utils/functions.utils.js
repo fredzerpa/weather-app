ff1c466ac6f4f68fad4920dcdb2cc3d26f5038db
TOR <52118178+fredzerpa@users.noreply.github.com>
2021-09-20 21:22:07 -0400

    Add new functions

--- src/utils/functions.utils.js
+++ src/utils/functions.utils.js
@@ -2,3 +2,22 @@ export const convertFarenheitToCelcius = temp => (temp - 32) * (5 / 9);
 
 export const getRandomNumber = (maxNumber = 1) =>
   Math.floor(Math.random() * maxNumber);
+
+export const getUserGeoLocation = async () => {
+  // ! Is promise function which will return the userLocation
+  if (!('geolocation' in navigator)) {
+    return 'Geolocation is not available on this device';
+  }
+  const askUserLocation = () =>
+    // * It's a promise so we can get the value in other sections of the code
+    new Promise((res, rej) => {
+      navigator.geolocation.getCurrentPosition(res, rej);
+    });
+  const userLocation = await askUserLocation();
+  return userLocation;
+};
+
+export const capitalizeFirstLetter = (
+  [first, ...rest],
+  locale = navigator.language
+) => first.toLocaleUpperCase(locale) + rest.join('');
